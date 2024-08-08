262e4c1f6c86e04fcc5e7bc3463dc00ec93cb817
Poul Kjeldager Sørensen <poul@kjeldager.com>
2024-08-08 11:07:25 +0000

    added missing namespace

--- templates/EAVFW/apps/__EAVFW__.__MainApp__/Startup.cs
+++ templates/EAVFW/apps/__EAVFW__.__MainApp__/Startup.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using EAVFW.Extensions.Documents;
 
 using __EAVFW__.__MainApp__.Infrastructure;
 using __EAVFW__.Common;
